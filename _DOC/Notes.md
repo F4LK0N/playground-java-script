8677544e788fbda9d8e7d4d4afcdbfae5ec3491f
F4lk0n <otaviosoria@gmail.com>
2023-06-08 03:53:58 -0300

    first commit

---
+++ _DOC/Notes.md
@@ -0,0 +1,5 @@
+https://www.w3schools.com/js/default.asp
+https://www.w3schools.com/js/js_intro.asp
+https://www.w3schools.com/js/js_whereto.asp
+https://www.w3schools.com/js/js_output.asp
+
---
+++ js/01-output.js
@@ -0,0 +1,13 @@
+//Overwrite Element HTMl
+document.getElementById("js-01-output").innerHTML = "Hello World!";
+
+//Overwrite All Document HTML
+//document.write("Overwrite all document HTML");
+
+//Alert Box
+//window.alert("Hello Alert!");
+
+//Console
+console.log("Hello Console Log");
+console.warn("Hello Console Warning");
+console.error("Hello Console Error");
